c66b3f1b0870041f3e50162b3d8eb42e2b40edac
urzuaf <urzua.fernando.f@gmail.com>
2024-07-06 19:43:47 -0400

    algoritmos

--- PlanificadorProcesos.java
+++ PlanificadorProcesos.java
@@ -1,7 +1,7 @@
 public class PlanificadorProcesos {
     // Atributos
     private static PlanificadorProcesos instancia; // Singleton
-    
+
     // Constructor
     private PlanificadorProcesos() {
     }
@@ -12,5 +12,39 @@ public class PlanificadorProcesos {
             instancia = new PlanificadorProcesos();
         }
         return instancia;
+    }
+
+    
+
+    public Proceso[] AlgoritmoFIFO(Proceso[] procesos) {
+        // FCFS o FIFO es el algoritmo más simple, pero el que menos rendimiento ofrece
+        // el primer proceso que llega se ejecuta, y una vez terminado se ejecuta el
+        // siguiente
+        // Al ser un FIFO no hay en si un algoritmo, sino que enviamos en el orden en
+        // que llega
+        return procesos;
+    }
+
+    public Proceso[] AlgoritmoSJF(Proceso[] procesos) {
+        // SJF, prioriza los procesos más cortos primero independientemente de su
+        // llegada
+        // en caso de que sean iguales usará FIFO. Tiene el problema de generar
+        // inanición para procesos largos
+        // comprobamos que exista el arreglo de procesos
+        if (procesos == null || procesos.length == 0) {
+            return procesos;
+        }
+
+        for (int i = 0; i < procesos.length - 1; i++) {
+            for (int j = 0; j < procesos.length - i - 1; j++) {
+                if (procesos[j].getSize() > procesos[j + 1].getSize()) {
+                    Proceso aux = procesos[j];
+                    procesos[j] = procesos[j + 1];
+                    procesos[j + 1] = aux;
+                }
+            }
+        }
+
+        return procesos;
     }
 }
--- Simulador.java
+++ Simulador.java
@@ -1,5 +1,36 @@
 public class Simulador{
     public static void main(String args[]){
-        System.out.println("hello world");
+
+        PlanificadorProcesos planificador = PlanificadorProcesos.getInstancia();
+
+        // imaginemos que estos seran los procesos
+
+        Proceso proceso1 = new Proceso(1, 3); 
+        Proceso proceso2 = new Proceso(2, 8); 
+        Proceso proceso3 = new Proceso(3, 5); 
+        Proceso proceso4 = new Proceso(4, 5); 
+        Proceso proceso5 = new Proceso(5, 10); 
+        Proceso proceso6 = new Proceso(6, 2); 
+
+        Proceso[] procesos = {proceso1, proceso2, proceso3};
+
+        Proceso[] procesosFIFO = planificador.AlgoritmoFIFO(procesos);
+
+        Proceso[] procesosSJF = planificador.AlgoritmoSJF(procesos);
+
+        System.out.println("Procesos por FIFO");
+        ImprimirProcesos(procesosFIFO);
+
+        System.out.println("Procesos por SFJ");
+        ImprimirProcesos(procesosSJF);
+
     }
+
+    public static void ImprimirProcesos(Proceso[] procesos){
+        for (int i=0; i<procesos.length; i++){
+            System.out.println("proceso: " + procesos[i].getId() + " tamaño: " + procesos[i].getSize());
+        }
+    }
+
 }
+
